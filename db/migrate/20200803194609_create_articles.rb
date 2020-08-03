cf84d37754647d7d292365284f54661d211327d4
asiimwedismas <asiimwedismas@gmail.com>
2020-08-03 23:01:41 +0300

    Mirgated the db

--- db/migrate/20200803194609_create_articles.rb
+++ db/migrate/20200803194609_create_articles.rb
@@ -2,6 +2,11 @@
 
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
-    create_table :articles, &:timestamps
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
   end
 end
